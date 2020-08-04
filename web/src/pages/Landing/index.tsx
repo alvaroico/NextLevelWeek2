ee3f135581e34b7ba851c717f74612ff2bb34f8a
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-03 22:17:07 -0300

    Ajustes

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -7,7 +7,7 @@ import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
-import './styles.css';
+import "./styles.css";
 
 function Landing() {
   return (
@@ -16,27 +16,29 @@ function Landing() {
         <div className="logo-container">
           <img src={logoImg} alt="Proffy" />
           <h2>Sua plataforma de estudos online.</h2>
-          <img
-            src={landingImg}
-            alt="Plataforma de estudos"
-            className="hero-image"
-          />
-          <div className="buttons-container">
-            <a href="http://" className="study">
-              <img src={studyIcon} alt="Estudar"/>
-              Estudar
-            </a>
-            <a href="http://" className="give-classes">
-              <img src={giveClassesIcon} alt="Dar aulas"/>
-              Estudar
-            </a>
-          </div>
-
-          <span className="total-connections">
-            Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo"/>
-          </span>
-
         </div>
+
+        <img
+          src={landingImg}
+          alt="Plataforma de estudos"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <a href="http://" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </a>
+          <a href="http://" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Estudar
+          </a>
+        </div>
+
+        <span className="total-connections">
+          Total de 200 conexões já realizadas{" "}
+          <img src={purpleHeartIcon} alt="Coração Roxo" />
+        </span>
       </div>
     </div>
   );
