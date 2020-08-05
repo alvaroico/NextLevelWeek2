6877a9ea8da35f992ee4f6fb82eec5908a9beabf
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-05 19:33:14 -0300

    Connections

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.get('/users', () => {
-  console.log('Acessou a rota')
-  //36:12
-})
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World " });
+});
 
 app.listen(3333);
