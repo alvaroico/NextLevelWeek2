b9802cbb2c479df79f4c3092639dc4b5dccf2c7e
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-05 22:41:41 -0300

    Filtro de profesores e aulas disponiveis agendamento

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -13,16 +13,27 @@ export default class ClassesController {
   async index(request: Request, response: Response) {
     const filters = request.query;
 
-    if(!filters.week_day || !filters.subject || !filters.time){
+    if (!filters.week_day || !filters.subject || !filters.time) {
       return response.status(400).json({
-        error: 'Voce precisa informa um filtro'
-      })
+        error: "Voce precisa informa um filtro",
+      });
     }
-  
-  const timeInMinutes = convertHourMinutes(filters.time as String)
 
-console.log(timeInMinutes)    
-  return response.send()
+    const timeInMinutes = convertHourMinutes(filters.time as string);
+
+    const classes = await db("classes")
+      .whereExists(function(){
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(filters.week_day) as number])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+      })
+      .where("classes.subject", "=", filters.subject as string)
+      .join("users", "classes.user_id", "=", "users.id")
+      .select(["classes.*", "users.*"]);
+    return response.json(classes);
   }
 
   async create(request: Request, response: Response) {
