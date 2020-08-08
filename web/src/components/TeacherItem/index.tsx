3eae520304b4e93675ae760f395a88d9c57afb6b
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-08 00:33:27 -0300

    Connection Profesor

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
 export interface Teacher {
   id: number;
@@ -19,6 +20,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id:  teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -36,10 +43,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/Hora
           <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
