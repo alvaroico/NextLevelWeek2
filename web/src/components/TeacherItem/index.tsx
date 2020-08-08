301b6b31900fe547ebf4e686ec4db1d7658d7458
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-08 00:19:48 -0300

    Buscar Profesores

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,38 +1,48 @@
-import React from 'react'
+import React from "react";
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import './styles.css'
+import "./styles.css";
 
-function TeacherItem(){
-  return(
-    <article className="teacher-item">
-    <header>
-      <img src="https://avatars3.githubusercontent.com/u/59286021?s=460&u=acc6d10e64940122bd2c3c64c52dcae0098d03bf&v=4" alt="Alvaroico"/>
-      <div>
-        <strong>Álvaro Ribeiro Pereira</strong>
-        <span>Química</span>
-      </div>
-    </header>
-
-    <p>
-    orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-    <br/><br/>
-    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
-    </p>
-
-    <footer>
-      <p>
-        Preço/Hora
-        <strong>R$ 80,00</strong>
-      </p>
-      <button type="button">
-        <img src={whatsappIcon} alt="Whatsapp"/>
-        Entrar em contato
-      </button>
-    </footer>
-  </article>
-  )
+export interface Teacher {
+  id: number;
+  subject: string;
+  cost: number;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
 }
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+
+      <p>{teacher.bio}</p>
+
+      <footer>
+        <p>
+          Preço/Hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
 
 export default TeacherItem;
