bf4db031b86162be4fe13c274faaa72bb692bbd4
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-07 22:28:33 -0300

    Add Novo Horário

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
 
-import warningIcon from '../../assets/images/icons/warning.svg';
+import warningIcon from "../../assets/images/icons/warning.svg";
 import Textarea from "../../components/Textarea";
 import Select from "../../components/Select";
 import "./styles.css";
 
 function TeacherForm() {
+
+  const [scheduleItems, setScheduleItems] = useState([
+    {week_day: 0,  from: "", to: "",}
+  ]);
+
+
+  function addNewScheduleItem() {
+    setScheduleItems([
+      ...scheduleItems,
+      {
+        week_day: 0,
+        from: "",
+        to: "",
+      }
+    ])
+  }
+
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader
@@ -27,20 +44,20 @@ function TeacherForm() {
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Select 
-            name="subject" 
+          <Select
+            name="subject"
             label="Matéria"
             options={[
-              {value: 'Artes', label: 'Artes'},
-              {value: 'Biologia', label: 'Biologia'},
-              {value: 'Ciências', label: 'Ciências'},
-              {value: 'Educação', label: 'Educação'},
-              {value: 'Física', label: 'Física'},
-              {value: 'Geografia', label: 'Geografia'},
-              {value: 'História', label: 'História'},
-              {value: 'Matemática', label: 'Matemática'},
-              {value: 'Português', label: 'Português'},
-              {value: 'Química', label: 'Química'},
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação", label: "Educação" },
+              { value: "Física", label: "Física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Português", label: "Português" },
+              { value: "Química", label: "Química" },
             ]}
           />
           <Input name="cost" label="Custo da sua hora por aula" />
@@ -49,39 +66,40 @@ function TeacherForm() {
         <fieldset>
           <legend>
             Horários disponíveis
-            <button type="button">
+            <button type="button" onClick={addNewScheduleItem}>
               + Novo Horário
             </button>
           </legend>
 
-            <div className="schedule-item">
-              <Select
-                name="week_day"
-                label="Dia da semana"
-                options={[
-                  { value: "0", label: "Domingo" },
-                  { value: "1", label: "Segunda-feira" },
-                  { value: "2", label: "Tersa-feira" },
-                  { value: "3", label: "Quarta-feira" },
-                  { value: "4", label: "Quinta-feira" },
-                  { value: "5", label: "Sexta-feira" },
-                  { value: "6", label: "Sábado" },
-                ]}
-              />
-              <Input name="from" label="Das" type="time" />
-              <Input name="to" label="Até" type="time" />
-            </div>
-
+          {scheduleItems.map((scheduleItem) => {
+            return (
+              <div key={scheduleItem.week_day} className="schedule-item">
+                <Select
+                  name="week_day"
+                  label="Dia da semana"
+                  options={[
+                    { value: "0", label: "Domingo" },
+                    { value: "1", label: "Segunda-feira" },
+                    { value: "2", label: "Tersa-feira" },
+                    { value: "3", label: "Quarta-feira" },
+                    { value: "4", label: "Quinta-feira" },
+                    { value: "5", label: "Sexta-feira" },
+                    { value: "6", label: "Sábado" },
+                  ]}
+                />
+                <Input name="from" label="Das" type="time" />
+                <Input name="to" label="Até" type="time" />
+              </div>
+            );
+          })}
         </fieldset>
 
         <footer>
           <p>
-            <img src={warningIcon} alt="Aviso importante"/>
+            <img src={warningIcon} alt="Aviso importante" />
             Importante! <br />
           </p>
-          <button type="button">
-            Salvar cadastro
-          </button>
+          <button type="button">Salvar cadastro</button>
         </footer>
       </main>
     </div>
