36816ddd7ad4fb489def6781aa8af6ef9d689b80
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-08 16:57:29 -0300

    Mobile Start

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import Landing from './src/pages/Landing';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello NLW </Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <Landing />
+    <StatusBar style="auto" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
