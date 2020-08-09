d760b176693e4d294f00f8de578784970e0ba25c
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-09 01:28:06 -0300

    Ajuste Cor menu TabBarIcon Focused

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -33,8 +33,14 @@ function StudyTabs() {
         component={TeacherList}
         options={{
           tabBarLabel: "Proffys",
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name="ios-easel" size={size} color={color} />;
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-easel"
+                size={size}
+                color={focused ? "#8257e5" : color}
+              />
+            );
           },
         }}
       />
@@ -43,8 +49,8 @@ function StudyTabs() {
         component={Favorites}
         options={{
           tabBarLabel: "Favoritos",
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name="ios-heart" size={size} color={color} />;
+          tabBarIcon: ({ color, size, focused }) => {
+            return <Ionicons name="ios-heart" size={size} color={focused ? "#8257e5" : color} />;
           },
         }}
       />
